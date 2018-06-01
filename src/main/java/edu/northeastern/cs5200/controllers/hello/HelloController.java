4af833a4d85c5c0af64f09b32d65c02df3660967
Your Name Here <suhani.ladani@gmail.com>
2018-05-31 22:16:42 -0400

    Integrating Spring Boot with MySQL

--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
@@ -1,11 +1,31 @@
 package edu.northeastern.cs5200.controllers.hello;
 
 
-import org.springframework.web.bind.annotation.RequestMapping; 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	@Autowired
+	 HelloRepository helloRepository;
+	 @RequestMapping("/api/hello/insert")
+	 public HelloObject insertHelloObject() {
+	  HelloObject obj = new HelloObject("Hello Suhani Ladani!");
+	  helloRepository.save(obj);
+	  return obj;
+	 }
+	 
+	 @RequestMapping("/api/hello/insert/{msg}")
+	 public HelloObject insertMessage(@PathVariable("msg") String message) {
+	      HelloObject obj = new HelloObject(message);
+	      helloRepository.save(obj);
+	      return obj;
+	 }
 	
 	@RequestMapping("/api/hello/string")
 	public String sayHello() {
@@ -17,6 +37,13 @@ public class HelloController {
 	public HelloObject sayHelloObject() {
 		HelloObject obj = new HelloObject("Hello Suhani Ladani");
 		return obj;
+	}
+	
+	@RequestMapping("/api/hello/select/all")
+	public List<HelloObject> selectAllHelloObjects() {
+	 List<HelloObject> hellos =
+	  (List<HelloObject>)helloRepository.findAll();
+	 return hellos;
 	}
 
 }
